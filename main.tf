efb5b6c3c784107263f3c4b0f49cbe36e0ad1182
Daniel Ciaglia <daniel.ciaglia@tier.app>
2021-02-15 21:28:37 +0100

    Revert "Revert "s3 endpoint be more specific""
    This reverts commit 2e1d07d74209a781cbf8cbd4ee0a8fea9b0fdead.

--- main.tf
+++ main.tf
@@ -848,7 +848,8 @@ resource "aws_route" "private_nat_gateway" {
 data "aws_vpc_endpoint_service" "s3" {
   count = var.create_vpc && var.enable_s3_endpoint ? 1 : 0
 
-  service = "s3"
+  service      = "s3"
+  service_type = "Gateway"
 }
 
 resource "aws_vpc_endpoint" "s3" {
